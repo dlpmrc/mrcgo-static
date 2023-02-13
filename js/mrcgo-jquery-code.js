1a4ec5338d9e6792b80aa9412185d435966c5079
orlandoMRC <118772732+orlandoMRC@users.noreply.github.com>
2023-02-13 17:19:26 -0600

    New Mobile Menu and UI/UX Updates
    - New mobile search UI/UX implemented.
    - Updated mobile menu UI/UX.
    -Font Awesome iconography integration.
    - Login modal integration.
    - Loading experience integration.
    - JS updates for prototype interaction.

--- js/mrcgo-jquery-code.js
+++ js/mrcgo-jquery-code.js
@@ -14,7 +14,6 @@ $(function () {
   // this initializes the dialog (and uses some common options)
   $("#loginModal").dialog({
     autoOpen: false,
-    //width: 600,
     width: '100%', // overcomes width:'auto' and maxWidth bug
     maxWidth: 500,
     height: 'auto',
@@ -33,14 +32,52 @@ $(function () {
     $("#loginModal").dialog("open");
     return false;
   });
+  // LOGIN LINK ONCLICK HANDLER (MOBILE)
+  $(".mobile-login-link").click(function () {
+    $("#loginModal").dialog("open");
+    return false;
+  });
 });
 // END LOGIN MODAL
+
+// START LOGIN REDIRECT EXPERINCE
+$('#login-button-cta').click(function (e) {
+  e.preventDefault(); //will stop the link href to call the blog page
+
+  setTimeout(function () {
+    window.location.href = "./logged-in/index.html"; //will redirect to your blog page (an ex: blog.html)
+  }, 2000); //will call the function after 2 secs.
+
+});
+// END LOGIN REDIRECT EXPERINCE
 
 // START LOADING EXPERIENCE / SPINNER
 setTimeout(function () {
   $(".loading-screen-container").hide()
 }, 3000);
 // END LOADING EXPERIENCE / SPINNER
+
+// START FOOTER VIDEO MODAL
+$(function () {
+  // this initializes the dialog (and uses some common options)
+  $("#footerVideoModal").dialog({
+    autoOpen: false,
+    //width: 600,
+    width: '100%', // overcomes width:'auto' and maxWidth bug
+    maxWidth: 800,
+    height: '400',
+    modal: true,
+    fluid: true, //new option
+    resizable: false,
+    draggable: false
+  });
+  // SHOPING CART LINK ONCLICK HANDLER 
+  $("#play-footer-video-modal").click(function () {
+    $("#footerVideoModal").dialog("open");
+    return false;
+  });
+});
+// END FOOTER VIDEO MODAL
 
 // START JS TO MAKE MODAL FLUID/RESPOSIVE
 // on window resize run function
@@ -74,9 +111,44 @@ function fluidDialog() {
       dialog.option("position", dialog.options.position);
     }
   });
-
 }
 // END JS TO MAKE MODAL FLUID/RESPOSIVE
+
+// START MOBILE MENU ACCORDION FOR SUB-MENU ITEMS
+$('.dropdown-menu').click(function (e) {
+  e.stopPropagation(); // will stop the accordion from closing the menu
+});
+$(function () {
+  $("#products-sub-item-accordion").accordion({
+    collapsible: true,
+    active: false,
+    icons: false,
+    heightStyle: "content"
+  });
+});
+// END MOBILE MENU ACCORDION FOR SUB-MENU ITEMS
+
+// START JS FOR SEARCH EXPAND/COLLAPSE COMPONENT
+$(".search-header").click(function () {
+
+  $header = $(this);
+  //getting the next element
+  $content = $header.next();
+  $content.slideToggle(100, function () {
+  });
+
+});
+// Toggle mobile search icon on click - Changes from magnifying glass to X icon to close
+$("button#top-search-icon-mobile").click(function (e) {
+  $('.search-header i.fa-solid.fa-magnifying-glass').not(this).toggleClass("fa-xmark");
+  e.preventDefault();
+})
+// Close search widget on overlay click - Change icon back to magnifying glass
+$("div.search-content:after").click(function (e) {
+  $('.search-header i.fa-solid.fa-xmark').not(this).toggleClass("fa-magnifying-glass");
+  e.preventDefault();
+})
+// END JS FOR SEARCH EXPAND/COLLAPSE COMPONENT
 
 // START JS FOR AUTOCOMPLTE & SPOTLIGHT SEARCH
 $(function () {
@@ -109,3 +181,5 @@ $(function () {
   });
 });
 // END JS FOR AUTOCOMPLTE & SPOTLIGHT SEARCH
+
+
